4f7e67256d6c79cfcfdb0dc2de0103ffbc463a2c
Glauco Veiga <veiga.cg@gmail.com>
2023-04-16 21:42:31 -0300

    Home

--- MiniBlog/src/pages/Home/Home.jsx
+++ MiniBlog/src/pages/Home/Home.jsx
@@ -1,11 +1,48 @@
-import styles from './Home.module.css' 
+import styles from './Home.module.css'
+
+// hooks
+import { useNavigate, Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+
+// components
+
 
 const Home = () => {
+  const [query, setQuery] = useState('');
+  const [posts] = useState([]);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>Veja nossos posts mais recentes</h1>
+      <form className={styles.search_form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+
+      <div className="post-list">
+        
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
-  )
-}
+    
+  )}
 
 export default Home
