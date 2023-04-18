ab6d5c2f2d67646d94c0bca2d2682a341318293d
Glauco Veiga <veiga.cg@gmail.com>
2023-04-18 12:26:05 -0300

    Query

--- MiniBlog/src/pages/Home/Home.jsx
+++ MiniBlog/src/pages/Home/Home.jsx
@@ -1,26 +1,33 @@
 import styles from './Home.module.css'
 
 // hooks
-import { useNavigate, Link } from 'react-router-dom'
+import { useNavigate, Link, Navigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import { useFetcDocuments } from '../../hooks/useFetchDocuments';
+import PostDetail from '../../components/PostDetail'
 
 // components
 
 
 const Home = () => {
   const [query, setQuery] = useState('');
-  const [posts] = useState([]);
+  const {documents: posts, loading } = useFetcDocuments('posts');
 
-  const navigate = useNavigate();
+  const Navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if(query) {
+      return Navigate(`/search?q=${query}`)
+    }
   };
 
 
   return (
     <div className={styles.home}>
       <h1>Veja nossos posts mais recentes</h1>
+      {/* form de pesquisa */}
       <form className={styles.search_form} onSubmit={handleSubmit}>
         <input
           type="text"
@@ -31,7 +38,10 @@ const Home = () => {
       </form>
 
       <div className="post-list">
-        
+        {loading && <p>Carregando...</p>}
+        {posts && posts.map((post) => (
+          <PostDetail key={post.id} post={post} />
+        ))}
         {posts && posts.length === 0 && (
           <div className={styles.noposts}>
             <p>Não foram encontrados posts</p>
---
+++ MiniBlog/src/pages/Search/Search.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './Search.module.css'
+
+// hooks
+import { useFetcDocuments } from '../../hooks/useFetchDocuments'
+import { useQuery } from '../../hooks/useQuery'
+
+const Search = () => {
+  const query = useQuery()
+  const search = query.get('q')
+  
+  return (
+    <div>
+      <h2>Search</h2>
+      <p>{search}</p>
+    </div>
+  )
+}
+
+export default Search
